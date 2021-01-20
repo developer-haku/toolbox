eb0a080a83d8aaa4fc72ba42aff7bc6d73812b54
developer-haku <51524534+developer-haku@users.noreply.github.com>
2021-01-19 22:08:19 -0500

    add tool page

---
+++ pages/tool/[tid].tsx
@@ -0,0 +1,23 @@
+import React, { createElement, useLayoutEffect, useState } from "react";
+import { useRouter } from "next/router";
+import DefaultLayout from "../../components/DefaultLayout/";
+import { Tools } from "../../components/Tools/";
+
+const ToolId = () => {
+  const router = useRouter();
+  const { tid } = router.query;
+  let toolIdValid =
+    !isNaN(parseInt(String(tid))) && Tools[String(tid)] ? true : false;
+
+  return (
+    <DefaultLayout breadcrumb={{ name: "tool" }}>
+      {toolIdValid ? (
+        createElement(Tools[String(tid)])
+      ) : (
+        <div>Cannot find the tool with id: {tid}</div>
+      )}
+    </DefaultLayout>
+  );
+};
+
+export default ToolId;
